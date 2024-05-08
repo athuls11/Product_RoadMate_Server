ef2eb445193227679462f34d5c218969747fb253
athuls11 <athuls180@gmail.com>
2024-05-09 00:42:26 +0530

    mysql connection with pool

--- db/connection.js
+++ db/connection.js
@@ -1,30 +1,27 @@
-// import pkg from "pg";
 import mysql from "mysql";
 
-// const { Client } = pkg;
-
-// const client = new Client({
-//   user: "default",
-//   host: "ep-dry-sea-a4o45xa6-pooler.us-east-1.aws.neon.tech",
-//   database: "verceldb",
-//   password: "ruE78VWeUnKx",
-//   port: 5432,
-//   ssl: true,
+// const db = mysql.createConnection({
+//   host: "sql5.freesqldatabase.com",
+//   user: "sql5704713",
+//   password: "XE7nNPWECP",
+//   database: "sql5704713",
+//   port: 3306,
 // });
 
-const db = mysql.createConnection({
+// db.connect((err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("MySQL Connected...");
+// });
+
+const db = mysql.createPool({
+  connectionLimit: 5,
   host: "sql5.freesqldatabase.com",
   user: "sql5704713",
   password: "XE7nNPWECP",
   database: "sql5704713",
   port: 3306,
-});
-
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("MySQL Connected...");
 });
 
 export { db };
