74434ba390dcf5333104a701a8d68e07c23dc8c2
athuls11 <athuls180@gmail.com>
2024-05-08 15:05:10 +0530

    commit

---
+++ prisma/migrations/20240508093336_create_product_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
